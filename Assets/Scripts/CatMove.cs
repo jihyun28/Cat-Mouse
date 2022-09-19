3715e59c65323194e3150bc4f6013455190b7a39
SOO <altnfsudwo@naver.com>
2022-09-20 01:02:47 +0900

    Art_Room_init

---
+++ Assets/Scripts/CatMove.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMove : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump")) //&& !anim.GetBool("isJumping") = 무한 점프 막기
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonUp("Horizontal")) //버튼에서 손을 때는 경우, 속력을 줄임
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        //방향 전환
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //Animation
+        if(Mathf.Abs(rigid.velocity.x) < 0.3) // 절대값 = abs
+        {
+            anim.SetBool("isMoving", false);
+        } else
+            anim.SetBool("isMoving", true);
+    }
+
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //maxSpeed보다 빠른 경우
+        //속도 관련한 벡터 -> velocity
+        if (rigid.velocity.x > maxSpeed) // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); 
+        else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        // Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down * 3, Color.red);
+            RaycastHit2D rayhit = Physics2D.Raycast(rigid.position, Vector3.down * 3, 3, LayerMask.GetMask("Platform"));
+
+            if (rayhit.collider != null)
+            {
+                if (rayhit.distance < 1.5f) // player 크기의 반 -> 크기 3으로 수정해서 1.5
+                {
+                    //Debug.Log(rayhit.collider.name);
+                    anim.SetBool("isJumping", false);
+                }
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Room")
+        {
+            //NextStage
+            gameManager.NextStage();
+        }
+
+    }
+
+    public void VelocityZero()
+    {
+        rigid.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int stageIndex;
+    public CatMove player;
+    public GameObject[] Stages;
+
+    public void NextStage()
+    {
+        Stages[stageIndex].SetActive(false);
+        stageIndex++;
+        Stages[stageIndex].SetActive(true);
+        PlayerReposition();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerReposition();
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(3, 5, 0);
+        player.VelocityZero();
+    }
+
+}
